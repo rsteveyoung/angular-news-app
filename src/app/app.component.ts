403ded9cf6626e765a751a189c670e77ac49428c
rsteveyoung <r.steve.young@gmail.com>
2018-10-06 21:58:37 -0300

    Adding main component code.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NewsApiService } from './news-api.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  mArticles:Array<any>;
+  mSources: Array<any>;
+
+  constructor(private newsApi: NewsApiService) {
+    console.log('App component constructor called');
+  }
+
+  ngOnInit() {
+    // Load articles
+    this.newsApi.initArticles()
+      .subscribe(data => this.mArticles = data['articles']);
+
+    // Load news sources
+    this.newsApi.initSources()
+      .subscribe(data => this.mSources = data['sources']);
+  }
+
+  searchArticles(source) {
+    console.log("Selected source is: " + source);
+    this.newsApi.getArticlesByID(source)
+      .subscribe(data => this.mArticles = data['articles']);
+  }
 }
